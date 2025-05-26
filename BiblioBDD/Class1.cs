3aabb513baf7851a3109c9427f22403d80741b08
florian <florian.raux10@gmail.com>
2025-05-26 15:37:10 +0200

    Ajout de la classe Connexion avec une méthode de connexion et de déconnexion + ajout de la librairie MySql

--- BiblioBDD/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace BiblioBDD
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ BiblioBDD/Connexion.cs
@@ -0,0 +1,49 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace BiblioBDD
+{
+    public class BDD
+    {
+        private static MySqlConnection conn;
+
+        public static bool OuvrirConnexion()
+        {
+            string serveur = "10.1.139.236";
+            string login = "e3";
+            string mdp = "mdp";
+            string bd = "basee3";
+
+            string chaineConnexion = $"server={serveur};uid={login};pwd={mdp};database={bd}";
+            try
+            {
+                conn = new MySqlConnection(chaineConnexion);
+                conn.Open();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine($"Erreur de connexion à la base de données : {ex.Message}");
+                return false;
+            }
+        }
+
+        public static bool FermerConnexion()
+        {
+            try
+            {
+                if (conn != null && conn.State == System.Data.ConnectionState.Open)
+                {
+                    conn.Close();
+                    conn.Dispose();
+                    return true;
+                }
+                return false;
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine($"Erreur lors de la fermeture de la connexion : {ex.Message}");
+                return false;
+            }
+        }
+    }
+}
